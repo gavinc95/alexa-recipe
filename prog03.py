74df0207a5a06eb52774788d19c86da57ab4f43a
Gavin Chan <gavinwhchan@gmail.com>
2017-03-17 00:42:25 -0700

    fixed db errors

--- prog03.py
+++ prog03.py
@@ -34,7 +34,6 @@ def on_intent(intent_request, session):
     intent = intent_request["intent"]
     intent_name = intent_request["intent"]["name"]
 
-    print 'intent NAME', 
     if intent_name == "start_directions_intent":
         return handle_start_directions_intent(intent, session)
     elif intent_name == "next_ingredient_intent":
@@ -140,7 +139,6 @@ def handle_next_step_intent(intent, session):
     global in_recipe, dir_index, dir_list, ingredient_list, ingredient_index, in_directions
 
     card_title = intent["name"]
-    # emergency_type = intent['slots']['help_call']['value']
     session_attributes = {}
     should_end_session = False
     speech_output = ""
@@ -177,7 +175,6 @@ def handle_last_ingredient_intent(intent, session):
     global in_recipe, dir_index, dir_list, ingredient_list, ingredient_index, in_directions
  
     card_title = intent["name"]
-    # emergency_type = intent['slots']['help_call']['value']
     session_attributes = {}
     should_end_session = False
     speech_output = ""
@@ -193,7 +190,6 @@ def handle_restart_intent(intent, session):
     global in_recipe, dir_index, dir_list, ingredient_list, ingredient_index, in_directions
 
     card_title = intent["name"]
-    # emergency_type = intent['slots']['help_call']['value']
     session_attributes = {}
     should_end_session = False
     speech_output = ""
@@ -224,7 +220,6 @@ def handle_options_intent(intent, session):
     global in_recipe, dir_index, dir_list, ingredient_list, ingredient_index, in_directions
 
     card_title = intent["name"]
-    # emergency_type = intent['slots']['help_call']['value']
     session_attributes = {}
     should_end_session = False
     speech_output = "You can say: quit or exit. You can also say: find recipe name where recipe name is the name of your recipe, I'd like to make recipe name, where recipe name is the name of your recipe."
@@ -239,26 +234,25 @@ def handle_food_choices_intent(intent, session):
     global in_recipe, dir_index, dir_list, ingredient_list, ingredient_index, in_directions
 
     card_title = intent["name"]
- 
     session_attributes = {}
     should_end_session = False
     speech_output = ""
 
     dynamodb = boto3.resource('dynamodb',
-                              # aws_session_token=aws_session_token,
-                              aws_access_key_id='AKIAIXDMBN2ENR6HG7UQ',
-                              aws_secret_access_key='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',
-                              region_name='us-east-1'
+      # aws_session_token=aws_session_token,
+      aws_access_key_id='AKIAIXDMBN2ENR6HG7UQ',
+      aws_secret_access_key='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',
+      region_name='us-east-1'
     )
 
     table = dynamodb.Table('RecipeList')
-
     data = table.scan().get('Items')
+    # print 'data', data
     food_type = intent['slots']['food_choice']['value']
 
-    # print 'RESPONSE AAAAAAAAAAAAAAAA', data
     found_recipe = False
     for i, recipe_dict in enumerate(data):
+        print recipe_dict['RecipeName'].lower(), str(food_type).lower(), 'aaaaaaaaaaaaaaaaaaaaaa'
         if recipe_dict['RecipeName'].lower() == str(food_type).lower():
             found_recipe = True
             in_recipe = True
